e88253ac9d624be0c8691043dfe12453b583a47e
shaikameena2109 <shaikfaizahmed321@gmail.com>
2026-06-10 17:05:33 +0530

    Sum_input.c

---
+++ Sum_input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int a, b;
+printf("enter first number: ");
+scanf("%d", &a);
+printf("enter second number: ");
+scanf("%d", &b);
+printf("sum = %d",a + b);
+return 0;
+}
